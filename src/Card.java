d8befd81cd76bce2ffe278e6ad5a67ff5debea09
25asheng <25asheng@castilleja.org>
2023-05-22 22:57:42 -0700

    finished Ms. Greyson method. Replaced names with properly displaying images: still unable to properly compare images

--- src/Card.java
+++ src/Card.java
@@ -1,13 +1,16 @@
+
+
+import processing.core.PImage;
+
 public class Card {
     private int x;
     private int y;
-    private String name;
+    private PImage img;
     private boolean isFlipped;
 
-    public Card(int x, int y, String theName){
+    public Card(int x, int y){
         this.x = x;
         this.y = y;
-        name = theName;
         isFlipped = false;
     }
 
@@ -22,12 +25,19 @@ public class Card {
             Main.app.fill(157, 159, 204);
             Main.app.rect(x, y, 100, 100);
 
-            Main.app.fill(30, 33, 94);
-            Main.app.textSize(20);
-            Main.app.text(name, x + 30, y + 60);
+            //Main.app.fill(30, 33, 94);
+            //Main.app.textSize(20);
+            Main.app.imageMode(Main.app.CENTER);
+            Main.app.image(img, x + 50, y + 50, 100, 100);
 
         }
     }
+
+
+    public void setupImage(String imageName) {
+        img = Main.app.loadImage(imageName);
+    }
+
 
     public void isRed(){
         Main.app.stroke(30, 33, 94);
@@ -36,7 +46,8 @@ public class Card {
 
         Main.app.fill(66, 8, 3);
         Main.app.textSize(20);
-        Main.app.text(name, x + 30, y + 60);
+        Main.app.imageMode(Main.app.CENTER);
+        Main.app.image(img, x + 50, y + 50, 100, 100);
     }
     public void isGreen(){
         Main.app.stroke(30, 33, 94);
@@ -45,21 +56,24 @@ public class Card {
 
         Main.app.fill(3, 66, 8);
         Main.app.textSize(20);
-        Main.app.text(name, x + 30, y + 60);
+        Main.app.imageMode(Main.app.CENTER);
+        Main.app.image(img, x + 50, y + 50, 100, 100);
     }
 
     public void isYellow(){
+        System.out.println("reached isYellow");
         Main.app.stroke(30, 33, 94);
-        Main.app.fill(15, 200, 20); //switch to yellow
+        Main.app.fill(237, 221, 71); //switch to yellow
         Main.app.rect(x, y, 100, 100);
 
-        Main.app.fill(3, 66, 8); //switch to yellow
+        Main.app.fill(156, 142, 11); //switch to yellow
         Main.app.textSize(20);
-        Main.app.text(name, x + 30, y + 60);
+        Main.app.imageMode(Main.app.CENTER);
+        Main.app.image(img, x + 50, y + 50, 100, 100);
     }
 
-    public String getName(){
-        return name;
+    public PImage getImg(){
+        return img;
     }
 
     public int getX(){
@@ -74,8 +88,8 @@ public class Card {
         return isFlipped;
     }
 
-    public void setName(String newName){
-        name = newName;
+    public void setImg(PImage newImg){
+        img = newImg;
     }
 
 
@@ -90,7 +104,10 @@ public class Card {
 
 
     public boolean equals(Card other){
-        return this.name.equals(other.getName());
+        System.out.println(this.img);
+        System.out.println(other.getImg());
+        System.out.println(this.img.equals(other.getImg()));
+        return this.img.equals(other.getImg());
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,17 @@ import processing.core.PApplet;
 import java.util.Random;
 import java.util.ArrayList;
 import java.lang.Math;
+import processing.core.PImage;
 
 public class Main extends PApplet{
     public static PApplet app;
     public Card[][] myCards = new Card[5][5];
-    private Card prevCard = new Card(0,0,"Lorem Ipsum!!");
-    private Card thisCard = new Card(0,0,"dolor.");;
+    private Card prevCard = new Card(0,0);
+    private Card thisCard = new Card(0,0);;
     private boolean onSecond = false;
-    private String[] names = {"Izzy", "Mika", "Bala", "Julia", "Kailee", "Caitlyn", "Ms.Greyson", "Michelle", "Claire", "Naya", "Anya", "Anaika","Norah"};
+    private String[] imgNames = {"Izzy.png", "Mika.png", "Bala.png", "Julia.png",
+            "Kailee.png", "Caitlyn.png", "Ms.Greyson.png", "Michelle.png",
+            "Claire.png", "Naya.png", "Anya.png", "Anaika.png","Norah.png"};
     private ArrayList<Card> greysonAdj = new ArrayList<Card>();
     private int countMoves;
     private int countDraw;
@@ -20,7 +23,6 @@ public class Main extends PApplet{
     private Card greyson;
     private int greysonX;
     private int greysonY;
-
 
     public static void main(String[] args){
         PApplet.main("Main");
@@ -41,20 +43,21 @@ public class Main extends PApplet{
         for(int i = 0; i < myCards.length; i++){
             for(int j = 0; j < myCards[0].length; j++){
                 int index = i * 5 + j;
+                myCards[i][j] = new Card(0,0);
                 if(index < 12){
-                    myCards[i][j] = new Card(0,0,names[index/2]);
+                    myCards[i][j].setupImage(imgNames[index/2]);
                 } else {
-                    myCards[i][j] = new Card(0,0,names[(index+1)/2]);
+                    myCards[i][j].setupImage(imgNames[(index+1)/2]);
 
                 }
             }
         }
-        myCards[2][2] = new Card(0,0,names[6]);
+        myCards[2][2].setupImage(imgNames[6]);
 
         scramble();
-
-
     }
+
+
 
     public void draw(){
         countDraw++;
@@ -93,9 +96,9 @@ public class Main extends PApplet{
 
 
     public void mouseClicked(){
-        System.out.println("mouseClicked");
+        //System.out.println("mouseClicked");
 
-        if(!isDone && !isWrong) {
+        if(!isDone && !isWrong && !clickGreyson) {
 
             for (int i = 0; i < myCards.length; i++) {
                 for (int j = 0; j < myCards[i].length; j++) {
@@ -103,7 +106,7 @@ public class Main extends PApplet{
                     if (!card.getIsFlipped()) {
                         boolean clickedCard = mouseX > card.getX() && mouseX < card.getX() + 100 && mouseY > card.getY() && mouseY < card.getY() + 100;
                         if (clickedCard) { //clicks on that Card
-                            if (card.getName().equals("Ms.Greyson")){
+                            if (card.getImg().equals(Main.app.loadImage(imgNames[6]))){
                                 clickGreyson = true;
                                 countDraw = 0;
                                 greyson = card;
@@ -138,20 +141,26 @@ public class Main extends PApplet{
             }
         }
 
-        System.out.println(onSecond);
+        //System.out.println(onSecond);
 
 
     }
 
     public void isRight(){
+        //frameRate(1);
         prevCard.isGreen();
         thisCard.isGreen();
+        /*
+        System.out.println("start delay");
+        delay(3500);
+        System.out.println("end delay");
+         */
+        //frameRate(50);
+
         countFound += 2;
     }
 
     public void clickGreyson(){
-        System.out.println(countFound);
-
         if(countDraw == 1){ //first time called
             System.out.println("reached countDraw == 1 for clickGreyson");
             greyson.setIsFlipped(true); //fix
@@ -164,38 +173,29 @@ public class Main extends PApplet{
             if(greysonX != 0){
                 greysonAdj.add(myCards[greysonX - 1][greysonY]);
             }
-
             if(greysonX != myCards.length - 1){
                 greysonAdj.add(myCards[greysonX + 1][greysonY]);
             }
-
             if(greysonY != 0){
                 greysonAdj.add(myCards[greysonX][greysonY - 1]);
             }
-
             if(greysonY != myCards[0].length - 1){
                 greysonAdj.add(myCards[greysonX][greysonY + 1]);
             }
-
-            for(Card adjCard: greysonAdj){
-                adjCard.isYellow();
-            }
-
         }
 
-        //turn surrounding yellow
-        //account for if sides
-        //if statement for each to determine if valid position
-        //can't do that within if loop first time because it'll get drawn over with-
-        //-the calls to display (normal color) repeated in draw
-
-
+        for(Card adjCard: greysonAdj){
+            adjCard.isYellow();
+            System.out.println("just made adjCard with x:" + adjCard.getX() + " and y: "+ adjCard.getY() + " yellow");
+        }
 
         if(countDraw % 100 == 0){
             //flip back cards that are not isFlipped
+            System.out.println("found countDraw % 100 == 0");
             countFound++;
             System.out.println(countFound);
             clickGreyson = false;
+            countDraw = 0;
         }
     }
 
@@ -241,8 +241,8 @@ public class Main extends PApplet{
 
         scramble();
 
-        prevCard = new Card(0,0,"Lorem Ipsum!!");
-        thisCard = new Card(0,0,"dolor.");;
+        prevCard = new Card(0,0);
+        thisCard = new Card(0,0);;
         onSecond = false;
         countMoves = 0;
         countFound = 0;
@@ -258,35 +258,6 @@ public class Main extends PApplet{
         }
 
     }
-
-    /*
-
-    public void infoDisplay(){
-        clickToRetry();
-        //and other text methods
-
-
-    }
-
-    private void clickToRetry(){
-        if(clickToRetry){
-            fill(160,120,255);
-            rect(290,695,170,44);
-
-            fill(60,0,80);
-            textAlign(CENTER);
-            textSize(25);
-            text("Click to retry!",375,725);
-        } else {
-            fill(262, 217, 222); //cover "Click to retry" box
-            noStroke();
-            rect(285,690,180,53);
-
-        }
-    }
-
-
-    */
 
 
 }
